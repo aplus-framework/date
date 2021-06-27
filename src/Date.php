25d5aef879bf134b37d73bab61d993d20f616a84
Natan Felles <natanfelles@gmail.com>
2021-06-27 15:20:09 -0300

    Coding Standard: Add header comments

--- src/Date.php
+++ src/Date.php
@@ -1,4 +1,13 @@
-<?php namespace Framework\Date;
+<?php
+/*
+ * This file is part of The Framework Date Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Framework\Date;
 
 use DateTime;
 use DateTimeImmutable;
