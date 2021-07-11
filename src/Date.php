c45b9f2c41842bfd15567973e3eab490541de683
Natan Felles <natanfelles@gmail.com>
2021-07-11 15:47:36 -0300

    Indent PHP files with space

--- src/Date.php
+++ src/Date.php
@@ -25,54 +25,54 @@ use Stringable;
  */
 class Date extends DateTime implements JsonSerializable, Stringable
 {
-	public const DATETIME = 'Y-m-d H:i:s';
+    public const DATETIME = 'Y-m-d H:i:s';
 
-	final public function __construct(string $datetime = 'now', DateTimeZone $timezone = null)
-	{
-		parent::__construct($datetime, $timezone);
-	}
+    final public function __construct(string $datetime = 'now', DateTimeZone $timezone = null)
+    {
+        parent::__construct($datetime, $timezone);
+    }
 
-	public function __toString() : string
-	{
-		return $this->format(static::ATOM);
-	}
+    public function __toString() : string
+    {
+        return $this->format(static::ATOM);
+    }
 
-	public function jsonSerialize() : string
-	{
-		return $this->format(static::ATOM);
-	}
+    public function jsonSerialize() : string
+    {
+        return $this->format(static::ATOM);
+    }
 
-	/**
-	 * Parse a string into a new static object according to the specified format.
-	 *
-	 * @param string $format Format accepted by date()
-	 * @param string $datetime A string representing the time
-	 * @param DateTimeZone|null $timezone A DateTimeZone object representing the
-	 * desired time zone
-	 *
-	 * @throws Exception Emits Exception in case of an error
-	 *
-	 * @return false|static
-	 */
-	public static function createFromFormat(
-		$format,
-		$datetime,
-		DateTimeZone $timezone = null
-	) : static | false {
-		$object = parent::createFromFormat($format, $datetime, $timezone);
-		return $object ? new static($object->format(static::ATOM)) : $object;
-	}
+    /**
+     * Parse a string into a new static object according to the specified format.
+     *
+     * @param string $format Format accepted by date()
+     * @param string $datetime A string representing the time
+     * @param DateTimeZone|null $timezone A DateTimeZone object representing the
+     * desired time zone
+     *
+     * @throws Exception Emits Exception in case of an error
+     *
+     * @return false|static
+     */
+    public static function createFromFormat(
+        $format,
+        $datetime,
+        DateTimeZone $timezone = null
+    ) : static | false {
+        $object = parent::createFromFormat($format, $datetime, $timezone);
+        return $object ? new static($object->format(static::ATOM)) : $object;
+    }
 
-	/**
-	 * @param DateTimeImmutable $object
-	 *
-	 * @throws Exception Emits Exception in case of an error
-	 *
-	 * @return static
-	 */
-	public static function createFromImmutable(DateTimeImmutable $object) : static
-	{
-		$object = parent::createFromImmutable($object);
-		return new static($object->format(static::ATOM));
-	}
+    /**
+     * @param DateTimeImmutable $object
+     *
+     * @throws Exception Emits Exception in case of an error
+     *
+     * @return static
+     */
+    public static function createFromImmutable(DateTimeImmutable $object) : static
+    {
+        $object = parent::createFromImmutable($object);
+        return new static($object->format(static::ATOM));
+    }
 }
--- tests/DateTest.php
+++ tests/DateTest.php
@@ -14,46 +14,46 @@ use PHPUnit\Framework\TestCase;
 
 final class DateTest extends TestCase
 {
-	protected Date $date;
+    protected Date $date;
 
-	public function setup() : void
-	{
-		$this->date = new Date();
-	}
+    public function setup() : void
+    {
+        $this->date = new Date();
+    }
 
-	public function testCreateFrom() : void
-	{
-		self::assertInstanceOf(
-			Date::class,
-			Date::createFromFormat('Y-m-d H:i:s', '2019-07-12 22:46:20')
-		);
-		self::assertInstanceOf(
-			Date::class,
-			Date::createFromImmutable(new \DateTimeImmutable())
-		);
-	}
+    public function testCreateFrom() : void
+    {
+        self::assertInstanceOf(
+            Date::class,
+            Date::createFromFormat('Y-m-d H:i:s', '2019-07-12 22:46:20')
+        );
+        self::assertInstanceOf(
+            Date::class,
+            Date::createFromImmutable(new \DateTimeImmutable())
+        );
+    }
 
-	public function testJsonSerialize() : void
-	{
-		self::assertSame(\json_encode(\date(\DATE_ATOM)), \json_encode($this->date));
-	}
+    public function testJsonSerialize() : void
+    {
+        self::assertSame(\json_encode(\date(\DATE_ATOM)), \json_encode($this->date));
+    }
 
-	public function testToString() : void
-	{
-		self::assertSame(\date(\DATE_ATOM), (string) $this->date);
-	}
+    public function testToString() : void
+    {
+        self::assertSame(\date(\DATE_ATOM), (string) $this->date);
+    }
 
-	public function testConstants() : void
-	{
-		self::assertSame(\date('Y-m-d H:i:s'), $this->date->format($this->date::DATETIME));
-	}
+    public function testConstants() : void
+    {
+        self::assertSame(\date('Y-m-d H:i:s'), $this->date->format($this->date::DATETIME));
+    }
 
-	public function testInstance() : void
-	{
-		$date = new Date();
-		$date = $date->setDate(2019, 12, 24);
-		self::assertInstanceOf(Date::class, $date);
-		$date = $date->setTime(15, 15);
-		self::assertInstanceOf(Date::class, $date);
-	}
+    public function testInstance() : void
+    {
+        $date = new Date();
+        $date = $date->setDate(2019, 12, 24);
+        self::assertInstanceOf(Date::class, $date);
+        $date = $date->setTime(15, 15);
+        self::assertInstanceOf(Date::class, $date);
+    }
 }
