db57fd2f86644eb938246c29f20939518e796235
Natan Felles <natanfelles@gmail.com>
2021-07-14 11:04:11 -0300

    Update default header comments

--- src/Date.php
+++ src/Date.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 /*
- * This file is part of The Framework Date Library.
+ * This file is part of Aplus Framework Date Library.
  *
  * (c) Natan Felles <natanfelles@gmail.com>
  *
